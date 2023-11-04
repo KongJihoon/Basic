ee0aaa5e250bf27e0f14879d6773ba0a9fc5cee9
공지훈 <138794635+KongJihoon@users.noreply.github.com>
2023-11-04 19:14:14 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,29 +1,8 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        String[] arr = myString.split("");
-        ArrayList<String> list = new ArrayList<>();
-        String result = "";
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i].equals("A")){
-                arr[i] = "B";
-                list.add(arr[i]);
-            }else if(arr[i].equals("B")){
-                arr[i] = "A";
-                list.add(arr[i]);
-            }
-        }
-        for (int i = 0; i < list.size(); i++) {
-            result += list.get(i);
-        }
-        if(result.contains(pat)){
-            answer = 1;
-        }
         
+        myString= myString.replace("A","a").replace("B","A").replace("a","B");
         
-        
-        return answer;
+        return myString.contains(pat) ? 1 : 0;
     }
 }
