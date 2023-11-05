8234639485d4b6948ccdbfa783e7c78ff67b6dad
공지훈 <138794635+KongJihoon@users.noreply.github.com>
2023-11-05 09:46:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 356 ms, Memory: 19200 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
