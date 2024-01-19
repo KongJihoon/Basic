da52e65487ff03881de3b2077f696ad549cb368d
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-01-19 10:17:07 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    static final long MOD = 1_000_000_000;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n ; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+
+
+
+
+    }
+}
