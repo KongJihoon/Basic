e5f3da0a0160b2b4aebae8fd35b91c1e58e50b5f
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-04-20 21:42:28 +0900

    [Bronze V] Title: 구구단, Time: 152 ms, Memory: 16168 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
 
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for (int i = 1; i <= 9 ; i++) {
-            System.out.println(num + " * " + i + " = " + (num * i));
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
         }
+
+
     }
+
 }
