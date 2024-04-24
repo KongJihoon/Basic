616984d9b79db3ad8128fde2da398885044f3c82
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-04-24 15:31:13 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 88016 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,21 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;        
+
         st = new StringTokenizer(br.readLine());
+
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+         
+            int value = Integer.parseInt(st.nextToken());
+            
+            if(max < value) {
+                max = value;
+            }
+            
+            if(min > value) {
+                min = value;
+            }
+            
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[arr.length - 1]);
+
+        System.out.println(min + " " + max);
+
 
     }
+
 }
