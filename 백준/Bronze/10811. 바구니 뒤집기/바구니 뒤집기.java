40c2ebac65b71ee6430094698d9262878c4affc9
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-04-25 22:43:27 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 156 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,37 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
+
+        int[] nums = IntStream.rangeClosed(1, n)
+                .toArray();
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
+
             int left = Integer.parseInt(st.nextToken()) - 1;
             int right = Integer.parseInt(st.nextToken()) - 1;
 
-            while (left < right){
-                int tmp = arr[left];
-                arr[left++] = arr[right];
-                arr[right--] = tmp;
+            while (left < right) {
+                int temp = nums[left];
+                nums[left++] = nums[right];
+                nums[right--] = temp;
             }
+
         }
-        br.close();
-        for (int item : arr){
-            System.out.print(item + " ");
+
+        for (int num : nums) {
+            System.out.print(num + " ");
         }
+
+
+
+
+
 
 
     }
+
 }
