43f4f052a9de454fe5c67665dc253d1520148b22
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-27 00:51:30 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,48 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+
+        String s = br.readLine();
+
         int cnt = 0;
 
-        for (int i = 0; i < str.length(); i++) {
 
-            switch (str.charAt(i)){
-                case 'A': case 'B': case 'C':
+        for (int i = 0; i < s.length(); i++) {
+
+            switch (s.charAt(i)) {
+
+                case 'A' : case 'B' : case 'C' :
                     cnt += 3;
                     break;
-                case 'D': case 'E': case 'F':
+                case 'D' : case 'E' : case 'F' :
                     cnt += 4;
                     break;
-                case 'G': case 'H': case 'I':
+                case 'G' : case 'H' : case 'I' :
                     cnt += 5;
                     break;
-                case 'J': case 'K': case 'L':
+                case 'J' : case 'K' : case 'L' :
                     cnt += 6;
                     break;
-                case 'M': case 'N': case 'O':
+                case 'M' : case 'N' : case 'O' :
                     cnt += 7;
                     break;
-                case 'P': case 'Q': case 'R': case 'S':
+                case 'P' : case 'Q' : case 'R' : case 'S' :
                     cnt += 8;
                     break;
-                case 'T': case 'U': case 'V':
+                case 'T' : case 'U' : case 'V' :
                     cnt += 9;
                     break;
-                case 'W': case 'X': case 'Y': case 'Z':
+                case 'W' : case 'X' : case 'Y' : case 'Z' :
                     cnt += 10;
                     break;
             }
 
+
+
         }
         System.out.println(cnt);
-
 
     }
 
