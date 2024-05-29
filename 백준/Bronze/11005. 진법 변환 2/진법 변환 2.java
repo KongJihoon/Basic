968e3f84f43ef74d6dafc7ffda90dc8c580d499b
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-30 02:15:40 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,19 +1,33 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
+
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        String result = Integer.toUnsignedString(s,n);
-        System.out.println(result.toUpperCase());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.toString(N, B).toUpperCase());
+
+
+
+
 
 
     }
+
+
+
+
+
+
 }
