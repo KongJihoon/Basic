6139c1fb88eb8b7a08d3176ef90863a9aafebf40
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-29 13:31:16 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 660 ms, Memory: 36444 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,36 +1,66 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arrA = new int[N][M];
-        int[][] arrB = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arrA[i][j] = Integer.parseInt(st.nextToken());
+
+
+
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] A = new int[n][m];
+        int[][] B = new int[n][m];
+
+
+        A = score(n, m);
+        B = score(n, m);
+
+        int[][] sum = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+                
+                sum[i][j] = A[i][j] + B[i][j];
             }
         }
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arrB[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(arrA[i][j] + arrB[i][j] + " ");
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+                System.out.print(sum[i][j] + " ");
             }
             System.out.println();
         }
 
+
     }
+
+    public static int[][] score(int n, int m) {
+
+        int[][] num = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+                 num[i][j] = sc.nextInt();
+            }
+
+        }
+        return num;
+    }
+
+
+
+
 }
