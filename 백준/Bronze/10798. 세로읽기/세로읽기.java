b262d9862c5fb952a561937e9a8422e22a1ff9bd
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-29 14:22:39 +0900

    [Bronze I] Title: 세로읽기, Time: 200 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,35 +1,51 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = 0;
-        char[][] c = new char[5][15];
-        for (int i = 0; i < c.length; i++) {
-            String s = br.readLine();
-            if(max < s.length()){
-                max = s.length();
-            }
-            for (int j = 0; j < s.length(); j++) {
-                c[i][j] = s.charAt(j);
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < max; i++) {
-            for (int j = 0; j < 5; j++) {
-                if(c[j][i] == '\0'){
-                   continue;
-                }
-                sb.append(c[j][i]);
-            }
-        }
-        System.out.println(sb.toString());
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
+
+        char[][] nums = chars();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < nums[0].length; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                if (nums[j][i] == '\0') continue;
+                sb.append(nums[j][i]);
+            }
+        }
+
+        System.out.println(sb);
 
     }
+
+    public static char[][] chars() {
+
+        char[][] chars = new char[5][15];
+
+
+
+        for (int i = 0; i < chars.length; i++) {
+            String s = sc.next();
+
+            for (int j = 0; j < s.length(); j++) {
+                chars[i][j] = s.charAt(j);
+            }
+
+        }
+        return chars;
+    }
+
+
+
+
 }
