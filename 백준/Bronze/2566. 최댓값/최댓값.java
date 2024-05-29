6bd3ac731c30b2a138df4ca450761fb5a153e5fb
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-29 13:41:52 +0900

    [Bronze III] Title: 최댓값, Time: 236 ms, Memory: 18488 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,36 +1,54 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int[][] arr = new int[9][9];
 
-        for (int i = 0; i < arr.length; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        int[][] nums = score();
 
-        int maxNum = Integer.MIN_VALUE;
-        int idxA = 0;
-        int idxB = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if(maxNum < arr[i][j]){
-                    maxNum = arr[i][j];
-                    idxA = i;
-                    idxB = j;
+        int max = Integer.MIN_VALUE;
+        int n = 0;
+        int m = 0;
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                if (max < nums[i][j]) {
+                   max = nums[i][j];
+                   n = i;
+                   m = j;
                 }
             }
         }
-        System.out.println(maxNum);
-        System.out.println((idxA + 1) + " " + (idxB + 1));
+        System.out.println(max);
+        System.out.println((n+1) + " " + (m + 1));
+
+
     }
+
+    public static int[][] score() {
+
+        int[][] num = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                 num[i][j] = sc.nextInt();
+            }
+
+        }
+        return num;
+    }
+
+
+
+
 }
