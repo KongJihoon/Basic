e54b7fa5365fe9c2f8fac5175f6f89d6d3568081
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-31 22:45:18 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,32 +1,46 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static boolean[] prime;
+
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        for (int i = 2; i <= Math.sqrt(N) ; i++) {
-            while (N % i == 0){
-                sb.append(i).append("\n");
-                N /= i;
+        int num = 2;
+
+        while (true) {
+
+            if(N == 1 || N == 0) {
+                break;
             }
+
+            while (N % num == 0) {
+
+                N = N / num;
+                System.out.println(num);
+            }
+
+            num++;
+
         }
 
-        if(N != 1){
-            sb.append(N);
-        }
 
-        System.out.println(sb);
+
+
+
 
     }
+
+
+
+
+
+
+
 }
