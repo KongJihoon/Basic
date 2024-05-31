7b506565c0c5d9f1326d499db17607a29b9fba90
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-31 18:54:30 +0900

    [Bronze I] Title: 약수들의 합, Time: 180 ms, Memory: 16508 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,39 +1,74 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
+
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
         while (true) {
+
             int n = Integer.parseInt(br.readLine());
-            ArrayList<Integer> list = new ArrayList<>();
-            if(n <= 0){
+
+            if (n == -1) {
                 break;
             }
 
-            for (int i = 1; i < n; i++) {
-                if (n % i == 0) {
-                    list.add(i);
-                }
+            ArrayList<Integer> list = nums(n);
+
+            int result = 0;
+
+            for (int i = 0; i < list.size(); i++) {
+
+                result += list.get(i);
+
+
             }
-            int sum = list.stream()
-                    .mapToInt(Integer::intValue)
-                    .sum();
-            if (n == sum) {
-                System.out.print(n + " = ");
+
+            if (result == n) {
+                String s = "";
                 for (int i = 0; i < list.size() - 1; i++) {
-                    System.out.print(list.get(i) + " + ");
+                    s += list.get(i) + " + ";
                 }
-                System.out.println(list.get(list.size() - 1));
-            }else {
+                s += list.get(list.size() - 1);
+
+                System.out.println(n + " = "  + s);
+
+            } else {
                 System.out.println(n + " is NOT perfect.");
             }
+
         }
 
+
+
     }
+
+    public static ArrayList<Integer> nums(int num) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i < num; i++) {
+
+            if(num % i == 0) {
+                list.add(i);
+            }
+
+        }
+
+        return list;
+    }
+
+
+
+
 }
