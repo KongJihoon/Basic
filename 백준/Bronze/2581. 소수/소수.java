529314c79f5c7690cc1cec3bfb00217858bcbc6e
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-05-31 22:35:07 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,52 +1,78 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
-    public static boolean[] prime;
+
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int M = Integer.parseInt(br.readLine());
-        int N = Integer.parseInt(br.readLine());
 
-        prime = new boolean[N + 1];
-        getPrime();
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int sum = 0;
+        boolean[] prime = isPrime(n + 1);
+
         int min = Integer.MAX_VALUE;
+        int sum = 0;
 
-        for (int i = M; i <= N; i++) {
-            if(prime[i] == false){
+        for (int i = m; i <= n; i++) {
+
+            if (!prime[i]) {
                 sum += i;
-                if(min == Integer.MAX_VALUE){
+
+                if(min == Integer.MAX_VALUE) {
                     min = i;
                 }
+
             }
         }
 
-        if(sum == 0){
+        if (sum == 0) {
             System.out.println(-1);
         }else {
             System.out.println(sum);
             System.out.println(min);
         }
 
+
+
+
     }
 
-    static void getPrime(){
+    static boolean[] isPrime(int num) {
+
+
+        boolean[] prime = new boolean[num + 1];
+
         prime[0] = true;
         prime[1] = true;
-        for (int i = 2; i < Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
 
-            for (int j = i * i; j < prime.length; j += i) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+
+            if (prime[i]) {
+               continue;
+            }
+
+            for (int j = i * i; j < num + 1; j= j+i) {
                 prime[j] = true;
             }
+
         }
+
+
+
+        return prime;
+
     }
+
+
+
+
+
+
 }
