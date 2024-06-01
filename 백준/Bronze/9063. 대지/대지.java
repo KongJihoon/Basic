47f076a0df0cf0297e37728a1d52761e1ffd9647
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-06-02 04:24:36 +0900

    [Bronze III] Title: 대지, Time: 288 ms, Memory: 26952 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,29 +1,59 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
+
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
 
-        int[] x = new int[n];
-        int[] y = new int[n];
-        for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                x[i] = Integer.parseInt(st.nextToken());
-                y[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int prev_x = Integer.parseInt(st.nextToken());
+        int prev_y = Integer.parseInt(st.nextToken());
+
+        int min_value_x = prev_x;
+        int max_value_x = prev_x;
+
+        int min_value_y = prev_y;
+        int max_value_y = prev_y;
+
+        while (n-- > 1) {
+
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int now_x = Integer.parseInt(st.nextToken());
+            int now_y = Integer.parseInt(st.nextToken());
+
+            max_value_x = Math.max(max_value_x, now_x);
+            min_value_x = Math.min(min_value_x, now_x);
+
+            max_value_y = Math.max(max_value_y, now_y);
+            min_value_y = Math.min(min_value_y, now_y);
         }
-        Arrays.sort(x);
-        Arrays.sort(y);
-        System.out.println((x[x.length - 1] - x[0]) * (y[y.length - 1] - y[0]));
+
+
+        int result_x = max_value_x - min_value_x;
+        int result_y = max_value_y - min_value_y;
+
+        System.out.println(result_x * result_y);
+
+
+
 
 
 
     }
+
+
+
+
+
+
+
 }
