42b497cf7a365c53c141797bc6d52a5cfa09fd9c
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2024-09-12 18:02:53 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 18384 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,82 +1,112 @@
-import java.io.*;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+class MyStack {
+
+    ArrayList list;
+
+    MyStack() {
+        this.list = new ArrayList();
+    }
+
+    public void push(int data) {
+
+        this.list.add(data);
+
+    }
+
+    public int pop() {
+
+        if (this.list.size() == 0) {
+            return -1;
+        }
+
+        int data = (int)this.list.get(this.list.size() - 1);
+
+        this.list.remove(this.list.size() - 1);
+
+
+        return data;
+    }
+
+    public int size() {
+
+        return this.list.size();
+    }
+
+    public int empty() {
+
+        if (this.list.size() == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+
+    public int top() {
+
+        if (this.list.size() == 0) {
+            return -1;
+        }
+
+        int data = (int)this.list.get(this.list.size() - 1);
+
+        return data;
+    }
+
+}
+
 
 public class Main {
 
-    public static int[] stack;
-    public static int size = 0;
+
+
 
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-
-        stack = new int[N];
         StringTokenizer st;
 
-        while (N-- > 0){
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        MyStack stack = new MyStack();
+
+        while (n-- > 0) {
+
             st = new StringTokenizer(br.readLine());
-            switch (st.nextToken()) {
 
-                case "push":
-                    push(Integer.parseInt(st.nextToken()));
-                    break;
-                case "pop":
-                    sb.append(pop()).append("\n");
-                    break;
-                case "size":
-                    sb.append(size()).append("\n");
-                    break;
-                case "empty":
-                    sb.append(empty()).append("\n");
-                    break;
-                case "top":
-                    sb.append(top()).append("\n");
-                    break;
+            String s = st.nextToken();
+
+            if (s.equals("push")) {
+                stack.push(Integer.parseInt(st.nextToken()));
+            } else if (s.equals("pop")) {
+                sb.append(stack.pop()).append("\n");
+            } else if (s.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (s.equals("empty")) {
+                sb.append(stack.empty()).append("\n");
+            } else {
+                sb.append(stack.top()).append("\n");
             }
+
+
         }
+
+
         System.out.println(sb);
-
-    }
-
-
-    public static void push(int item){
-        stack[size] = item;
-        size++;
-    }
-
-    public static int pop(){
-        if(size == 0){
-            return -1;
-        }else {
-            int result = stack[size - 1];
-            stack[size - 1] = 0;
-            size--;
-            return result;
-        }
-    }
-
-    public static int size(){
-        return size;
-    }
-
-    public static int empty(){
-        if(size == 0){
-            return 1;
-        }else {
-            return 0;
-        }
-    }
-
-    public static int top(){
-        if(size == 0){
-            return -1;
-        }else {
-            return stack[size - 1];
-        }
     }
 
 
 }
+
+
