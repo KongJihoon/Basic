124426b50f03b1e5e1d0c2012796e2840ba57f46
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-02-15 16:12:03 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,42 +1,67 @@
-import java.io.*;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
 
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
+
+
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            sb.append(isStack(s)).append("\n");
+
+        while (n-- > 0) {
+
+            String isVps = br.readLine();
+
+            Stack<String> stack = new Stack<>();
+
+            boolean checkVps = true;
+
+            for (String s : isVps.split("")) {
+
+                if (s.equals("(")) {
+                    stack.push(s);
+                } else if (s.equals(")")) {
+                    if (stack.isEmpty()) {
+                        checkVps = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+
+            }
+
+            if (stack.isEmpty() && checkVps) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+
+
         }
+
         System.out.println(sb);
 
-    }
 
 
-    public static String isStack(String item){
-        if(item.length() == 0){
-            return null;
-        }
-        Stack<Character> stack = new Stack();
-        boolean vpsCheck = true;
-        for (char c : item.toCharArray()){
-            if(c == '('){
-                stack.push(c);
-            } else if (stack.isEmpty()) {
-                vpsCheck = false;
-                break;
-            } else {
-                stack.pop();
-            }
-        }
 
-        return stack.isEmpty() && vpsCheck ? "YES" : "NO";
+
+
     }
 
 }
+
+
+
+
+
