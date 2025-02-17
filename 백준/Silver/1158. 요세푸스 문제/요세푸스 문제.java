7a3725edd7f9716b4db66bc00dcf0a91bbf472ce
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-02-17 20:22:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 592 ms, Memory: 294736 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,43 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
+
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
 
         Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.offer(i + 1);
+        }
+        int k = Integer.parseInt(st.nextToken());
+
         StringBuilder sb = new StringBuilder();
 
-        IntStream.range(1, n + 1).forEach(x -> queue.add(x));
-        ArrayList<Integer> list = new ArrayList<>();
-
-        int cnt = 0;
-        while (!queue.isEmpty()){
-
-            int data = (int) queue.remove();
-            cnt++;
-
-            if(cnt % k == 0){
-
-                list.add(data);
-            }else {
-                queue.add(data);
-            }
-        }
         sb.append("<");
-        for (int i = 0; i < list.size() - 1; i++) {
-            sb.append(list.get(i)).append(", ");
+
+        while (queue.size() != 1) {
+
+            for (int i = 0; i < k - 1; i++) {
+                queue.offer(queue.poll());
+            }
+
+            sb.append(queue.poll()).append(", ");
         }
-        sb.append(list.get(list.size() - 1)).append(">");
+
+        sb.append(queue.poll()).append(">");
+
         System.out.println(sb);
     }
+
+
+
+
+
 }
+
+
+
+
+
