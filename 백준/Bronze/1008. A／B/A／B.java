9ac17ebdd908cb0b9b1f2c4150af9d38d6c32d98
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-05-02 18:36:01 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,19 @@
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+       Scanner sc = new Scanner(System.in);
+
+
+
+       double a = sc.nextDouble();
+       double b = sc.nextDouble();
+       System.out.println(a / b);
+
+
+    }
+}
