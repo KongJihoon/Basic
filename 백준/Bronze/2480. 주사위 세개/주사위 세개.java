dd587b55ec69ada424c9cec7fe6fa73769d2a572
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-05-11 18:50:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,32 +1,33 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        int sum = 0;
-
         if (a == b && b == c) {
-            sum = 10000 + (a * 1000);
-        } else if (a == b || a == c) {
-            sum = 1000 + (a * 100);
+            System.out.println(10000 + (a * 1000));
+        } else if (a == b) {
+            System.out.println(1000 + (b * 100));
+        } else if (a == c) {
+            System.out.println(1000 + (c * 100));
         } else if (b == c) {
-            sum = 1000 + (b * 100); 
+            System.out.println(1000 + (c * 100));
         } else {
-            sum = Math.max(a, Math.max(b, c)) * 100;
+            int max = Math.max(Math.max(a, b), c);
+            System.out.println(max * 100);
         }
 
-        System.out.println(sum);
-
     }
-
 }
