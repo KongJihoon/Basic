cccb7df42a9d63377106300747f5a0a8b42557fb
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-06-01 18:26:42 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 15004 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,18 +6,23 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        int sum = 0;
-
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
+        int sum = sum(n);
 
         System.out.println(sum);
+    }
+
+    public static int sum(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return n + sum(n - 1);
 
     }
 
