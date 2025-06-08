11a1d0d01489eff119d37b1d180a54fea0fa2ccf
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-06-08 15:00:56 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,25 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringBuilder sb = new StringBuilder();
+
+
         int[] arr = new int[9];
-        for (int i = 0; i < arr.length; i++) {
+
+        int idx = 0;
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int max = Integer.MIN_VALUE;
-        int maxIdx = -1;
+
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > max){
+
+            if (arr[i] > max) {
+                idx = i;
+
                 max = arr[i];
-                maxIdx = i;
             }
+
         }
-        System.out.println(max + "\n" + (maxIdx + 1));
+
+        sb.append(arr[idx]).append("\n").append(idx + 1);
+
+        System.out.println(sb);
+
+
+
+
+
 
     }
+
+
+
 }
