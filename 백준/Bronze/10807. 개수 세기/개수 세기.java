f8ffb38b6cb59ca6be013b15287ada4620bc3a51
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-06-08 14:48:31 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,21 +1,38 @@
-import java.io.BufferedReader;
 
-import java.io.InputStreamReader;
-
+import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Arrays.setAll(arr, i -> Integer.parseInt(st.nextToken()));
 
         int v = Integer.parseInt(br.readLine());
+
         int cnt = 0;
+
         for (int i = 0; i < arr.length; i++) {
-            if(Integer.parseInt(arr[i]) == v){
+
+            if (arr[i] == v) {
                 cnt++;
             }
+
         }
+
         System.out.println(cnt);
+
     }
+
+
+
 }
