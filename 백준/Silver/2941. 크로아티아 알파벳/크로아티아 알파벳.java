1334927ce478357eb8203ecbdfbf5885863df72d
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-06-21 16:44:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,21 +1,108 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-        String s = br.readLine();
-        for (int i = 0; i < arr.length; i++) {
-            if(s.contains(arr[i])){
-                s = s.replace(arr[i],"!");
+        String input = br.readLine();
+
+        int cnt = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char ch = input.charAt(i);
+
+            if (ch == 'c') {
+
+                if (i < input.length() - 1) {
+
+                    if (input.charAt(i + 1) == '=') {
+                        i++;
+                    } else if (input.charAt(i + 1) == '-') {
+                        i++;
+                    }
+
+                }
+
+            } else if (ch == 'd') {
+                if (i < input.length() - 1) {
+
+
+                    if (input.charAt(i + 1) == 'z') {
+                        if (i < input.length() - 2) {
+                            if (input.charAt(i + 2) == '=') {
+                                i+=2;
+                            }
+                        }
+
+                    } else if (input.charAt(i + 1) == '-') {
+                        i++;
+                    }
+
+                }
+
+            } else if (ch == 'l') {
+                if (i < input.length() - 1) {
+
+                    if (input.charAt(i + 1) == 'j') {
+                        i++;
+                    }
+
+                }
+            } else if (ch == 'n') {
+                if (i < input.length() - 1) {
+                    if (input.charAt(i + 1) == 'j') {
+                        i++;
+                    }
+                }
+            } else if (ch == 's') {
+                if (i < input.length() - 1) {
+                    if (input.charAt(i + 1) == '=') {
+                        i++;
+                    }
+                }
+
+            } else if (ch == 'z') {
+                if (i < input.length() - 1) {
+                    if (input.charAt(i + 1) == '=') {
+                        i++;
+                    }
+                }
             }
+
+            cnt++;
         }
-        System.out.println(s.length());
+
+
+        System.out.println(cnt);
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
+
+
+
+
+
 }
