3d19ef0f7d729f646bf32b86817b11990e6b3b74
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-07-05 21:01:55 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,27 +6,19 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String s = st.nextToken();
+        int n = Integer.parseInt(st.nextToken());
 
-        String N = st.nextToken();
-        int B = Integer.parseInt(st.nextToken());
-
-        int result = Integer.parseInt(N, B);
-
-        System.out.println(result);
-
+        System.out.println(Integer.parseInt(s, n));
 
     }
 
-
-
-
-
-
 }
+
