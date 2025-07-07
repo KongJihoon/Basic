c7475356ec52ac6e5b95d87e78be9eaaebf1a52f
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-07-07 23:07:57 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,31 +1,41 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        int count = 1; // 거리
-        int range = 2; // 범위
+
+
+        int range = 2;
+
+        int cnt = 1;
+
+
 
         if (n == 1) {
-            System.out.println(1);
+            cnt = 1;
         } else {
+
             while (range <= n) {
-                range = range + (count * 6);
-                count++;
+
+                range = range + (6 * cnt);
+                cnt++;
             }
-            System.out.println(count);
+
         }
+
+        System.out.println(cnt);
 
     }
 
-
 }
+
