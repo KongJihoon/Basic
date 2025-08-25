b4890d40c098a990845d83f2de2154a482860c0c
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-08-25 20:20:39 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -2,77 +2,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-
+    public static boolean prime[];
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
         int m = Integer.parseInt(br.readLine());
+
         int n = Integer.parseInt(br.readLine());
 
-        boolean[] prime = isPrime(n + 1);
+        prime = new boolean[n + 1];
+        getPrime();
 
-        int min = Integer.MAX_VALUE;
         int sum = 0;
 
+        int min = Integer.MAX_VALUE;
         for (int i = m; i <= n; i++) {
 
             if (!prime[i]) {
                 sum += i;
-
-                if(min == Integer.MAX_VALUE) {
+                if (min == Integer.MAX_VALUE) {
                     min = i;
                 }
 
             }
+
         }
 
         if (sum == 0) {
             System.out.println(-1);
-        }else {
+        } else {
+
             System.out.println(sum);
             System.out.println(min);
+            
         }
-
-
 
 
     }
 
-    static boolean[] isPrime(int num) {
 
-
-        boolean[] prime = new boolean[num + 1];
+    static void getPrime() {
 
         prime[0] = true;
         prime[1] = true;
 
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= Math.sqrt(prime.length) ; i++) {
 
             if (prime[i]) {
-               continue;
+                continue;
             }
+            for (int j = i * i; j < prime.length; j+=i) {
 
-            for (int j = i * i; j < num + 1; j= j+i) {
                 prime[j] = true;
             }
-
         }
 
-
-
-        return prime;
-
     }
-
-
-
-
-
-
 }
