08a75946b04504666a60845fb54b23f5a15c8896
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-08-25 19:34:10 +0900

    [Bronze III] Title: 약수 구하기, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,30 +1,34 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int n = Integer.parseInt(st.nextToken());
+
         int k = Integer.parseInt(st.nextToken());
-        int cnt = 0;
+
+        List<Integer> list = new ArrayList<>();
+
         for (int i = 1; i <= n; i++) {
-            if(n % i == 0){
-                cnt++;
-                if(cnt == k){
-                    System.out.println(i);
-                    break;
-                }
+            if (n % i == 0) {
+                list.add(i);
             }
         }
-        if(cnt != k){
-            System.out.println(0);
-        }
 
+        if (list.size() < k) {
+            System.out.println(0);
+        } else {
+            System.out.println(list.get(k - 1));
+        }
 
     }
 }
