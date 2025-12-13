9d5d68043280848b0266f8b95b81d6b9f0e9db69
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2025-12-13 15:45:32 +0900

    [level 1] Title: 체육복, Time: 0.87 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 0.55 ms
+메모리: 82.8 MB, 시간: 0.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 11일 21:32:37
+2025년 12월 13일 15:45:29
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,40 +1,57 @@
-import java.util.*;
-
+import java.util.Arrays;
+import java.util.List;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-
         int answer = n - lost.length;
-
+        
+        boolean[] used = new boolean[reserve.length];
+        
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
-        // 1) 자기 자신이 여벌을 가진 경우 처리
-        boolean[] used = new boolean[reserve.length];
-
+        
+        // 잃어버렸지만 여벌옷을 챙겨온 인원 체크
         for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j] && !used[j]) {
-                    used[j] = true;
-                    lost[i] = -1; // lost에서 제거
-                    answer++;
-                    break;
-                }
-            }
-        }
-
-        // 2) 앞번호 → 뒤번호 순으로 빌리기
-        for (int i = 0; i < lost.length; i++) {
-            if (lost[i] == -1) continue;
 
             for (int j = 0; j < reserve.length; j++) {
-                if (!used[j] && (reserve[j] == lost[i] - 1 || reserve[j] == lost[i] + 1)) {
+                
+                if (lost[i] == reserve[j]) {
+                    
+                    lost[i] = -1;
                     used[j] = true;
                     answer++;
                     break;
                 }
+            
             }
+            
         }
 
+        for (int i = 0; i < lost.length; i++) {
+            
+            if (lost[i] == -1) {
+                continue;
+            }
+
+            for (int j = 0; j < reserve.length; j++) {
+                
+                
+                if (!used[j] && (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j])) {
+                    
+                    used[j] = true;
+                    answer++;
+                    break;
+
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+        
+        
         return answer;
     }
 }
