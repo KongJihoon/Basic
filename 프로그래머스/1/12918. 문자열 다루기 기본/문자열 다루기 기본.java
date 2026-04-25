c64579ce8cac66da199a97365943978b7f80b4ea
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-04-25 20:47:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,23 +1,22 @@
-
 class Solution {
     public boolean solution(String s) {
-
-
-        if(s.length() == 4 || s.length() == 6) {
-
+        
+        
+        
+        if ((s.length() == 4 || s.length() == 6)) {
+            
             for (char c : s.toCharArray()) {
-
+                
+                
                 if (!Character.isDigit(c)) {
                     return false;
                 }
-
+                
             }
-
-        } else {
-            return false;
-        } 
-
-
-        return true;
+            
+            return true;
+        }
+        
+        return false;
     }
 }
