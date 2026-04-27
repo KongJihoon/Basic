3ba46143d62702089c6e282f92bbbe82828f5d89
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-04-27 22:46:41 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,35 +1,28 @@
-
 class Solution {
-    public static String solution(String s) {
-        StringBuilder sb = new StringBuilder();
+    public String solution(String s) {
 
+        StringBuilder sb = new StringBuilder();
 
         int idx = 0;
 
         for (char c : s.toCharArray()) {
 
-
             if (c == ' ') {
-                sb.append(' ');
+                sb.append(c);
                 idx = 0;
             } else {
-
-                sb.append((idx++ % 2 == 0) ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                if (idx % 2 == 0) {
+                    sb.append(Character.toUpperCase(c));
+                    idx++;
+                } else  {
+                    sb.append(Character.toLowerCase(c));
+                    idx++;
+                }
             }
-
 
         }
 
 
-
-
         return sb.toString();
-    }
-
-
-    public static void main(String[] args) {
-        String s = "try hello world";
-
-        System.out.println(solution(s));
     }
 }
