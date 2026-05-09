b7a8870d80de7f2f8458a0d0a60ad380f7d55823
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-05-09 16:57:57 +0900

    [level 1] Title: 체육복, Time: 0.36 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,57 +1,62 @@
 import java.util.Arrays;
-import java.util.List;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
-        
-        boolean[] used = new boolean[reserve.length];
-        
+
         Arrays.sort(lost);
         Arrays.sort(reserve);
-
         
-        // 잃어버렸지만 여벌옷을 챙겨온 인원 체크
+        int answer = n - lost.length;
+
+        boolean[] isFlag = new boolean[31];
+
         for (int i = 0; i < lost.length; i++) {
 
             for (int j = 0; j < reserve.length; j++) {
-                
-                if (lost[i] == reserve[j]) {
-                    
-                    lost[i] = -1;
-                    used[j] = true;
+
+                if(lost[i] == reserve[j]) {
+                    isFlag[reserve[j]] = true;
                     answer++;
                     break;
                 }
-            
+
             }
-            
+
         }
 
+
         for (int i = 0; i < lost.length; i++) {
-            
-            if (lost[i] == -1) {
+
+
+            if (isFlag[lost[i]]) {
                 continue;
             }
 
             for (int j = 0; j < reserve.length; j++) {
-                
-                
-                if (!used[j] && (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j])) {
-                    
-                    used[j] = true;
+
+
+                if (reserve[j] == lost[i] + 1 || reserve[j] == lost[i] - 1) {
+
+                    if (isFlag[reserve[j]]) {
+                        continue;
+                    }
+
                     answer++;
+
+                    isFlag[reserve[j]] = true;
+
                     break;
 
-                    
                 }
-                
+
             }
-            
+
+
         }
-        
-        
-        
-        
+
+
+
+
         return answer;
     }
 }
