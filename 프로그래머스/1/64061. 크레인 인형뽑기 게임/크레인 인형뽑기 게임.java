7c9cacc651b4c87b9f06289f10ee04106db00d71
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-05-11 19:57:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.90 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -6,43 +6,33 @@ class Solution {
 
         Stack<Integer> stack = new Stack<>();
 
-        int[][] reverse = new int[board[0].length][board.length];
+        for (int move : moves) {
+            
+            int col = move - 1;
 
-        for (int i = 0; i < board.length; i++) {
-
-
-            for (int j = 0; j < board[i].length; j++) {
-
-                reverse[j][i] = board[i][j];
-
-            }
-        }
-
-
-        for (int i = 0; i < moves.length; i++) {
-
-            int line = moves[i] - 1;
-
-            for (int j = 0; j < reverse[line].length; j++) {
-
-                if (reverse[moves[i] - 1][j] == 0) {
+            for (int row = 0; row < board.length; row++) {
+                
+                if (board[row][col] == 0) {
                     continue;
                 }
-
-                if (!stack.isEmpty() && stack.peek() == reverse[line][j]) {
+                
+                int doll = board[row][col];
+                
+                if (!stack.isEmpty() && stack.peek() == doll) {
                     stack.pop();
-                    answer += 2;
-
+                    answer+=2;
                 } else {
-                    stack.push(reverse[line][j]);
-
+                    stack.push(doll);
                 }
-
-                reverse[line][j] = 0;
+                
+                
+                board[row][col] = 0; 
+                
                 break;
-
+                
             }
-
+            
+            
         }
 
 
