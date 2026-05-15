0c45e1408be99048a372420d52de5cf4296782ca
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-05-15 16:26:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+
+        for (int i = 0; i < absolutes.length; i++) {
+            
+            int num = absolutes[i];
+            
+            if (!signs[i]) {
+                answer -= num;
+                continue;
+            }
+            
+            answer += num; 
+            
+            
+        }
+        
+        return answer;
+    }
+}
