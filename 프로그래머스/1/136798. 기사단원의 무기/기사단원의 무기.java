ceba07e59c7415bf65cee406e8ede2f7cd10c8b0
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-05-27 12:26:07 +0900

    [level 1] Title: 기사단원의 무기, Time: 3908.38 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,54 +1,47 @@
-
 class Solution {
+    public int solution(int number, int limit, int power) {
 
-  
-
-
-    public static int solution(int number, int limit, int power) {
-
-
-        int weight = 1;
-
-        for (int i = 2; i <= number; i++) {
-
-            int newPower = getPower(i);
+        // 각 기사에게 1번부터 number까지 번호가 지정
+        // 각 기사는 자신의 기사 번호의 약수 개수에 해당하는 공격력을 가진 무기를 구매하려함
+        // 단, 제한수치보다 큰 공격력을 가진 무기를 구매해야 하는 기사는 정해진 공격력을 가진 무기를 구매해야한다.
 
 
-            if (newPower > limit) {
-                weight += power;
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            
+            int factorCnt = getFactor(i);
+            
+            if (factorCnt > limit) {
+                answer += power;
                 continue;
             }
-
-            weight += newPower;
-
+            
+            answer += factorCnt;
+            
         }
 
-        return weight;
+
+        return answer;
     }
 
 
-    public static int getPower(int number) {
-        int cnt = 0;
+    public int getFactor(int num) {
 
-        for (int i = 1; i * i <= number; i++) {
-            if (number % i == 0) {
-                cnt++;               
-                if (i != number / i) {
-                    cnt++;
-                }
+        int result = 0;
+
+        for (int i = 1; i <= num / 2 ; i++) {
+
+            if (num % i == 0) {
+                result++;
             }
+
+
         }
-        return cnt;
+
+        result++;
+
+
+        return result;
     }
-
-    public static void main(String[] args) {
-
-        int number = 5;
-        int limit = 3;
-        int power = 2;
-
-        System.out.println(solution(number, limit, power));
-
-    }
-
 }
