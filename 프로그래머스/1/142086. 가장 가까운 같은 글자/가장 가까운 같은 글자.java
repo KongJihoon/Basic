e6463d3087e6225ddd5503614cbf062a0832ec48
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-05-29 15:18:29 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 42.46 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,35 +1,30 @@
+import java.util.Arrays;
 
-import java.util.*;
 class Solution {
-    public static int[] solution(String s) {
+    public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        int[] lastIndex = new int[26];
 
-        Arrays.fill(lastIndex, -1);
+        Arrays.fill(answer, -1);
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = answer.length - 1; i >= 0; i--) {
 
-            char c = s.charAt(i);
+            char itemA = s.charAt(i);
 
-            int idx = c - 'a';
-
-            if (lastIndex[idx] == -1) {
-                answer[i] = -1;
-            } else {
-                answer[i] = i - lastIndex[idx];
+            for (int j = 0; j < i; j++) {
+                
+                char itemB = s.charAt(j);
+                
+                if (itemA == itemB) {
+                    answer[i] = i - j;
+                }
+                
             }
-
-            lastIndex[idx] = i;
+            
+            
+            
         }
 
+
         return answer;
-    }
-
-    public static void main(String[] args) {
-
-        String s = "banana";
-
-        System.out.println(Arrays.toString(solution(s)));
-
     }
 }
