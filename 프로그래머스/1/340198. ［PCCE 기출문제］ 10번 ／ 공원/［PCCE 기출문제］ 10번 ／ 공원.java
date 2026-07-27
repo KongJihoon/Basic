e529d0d7a689bc32bc09d1963e003d64174fdede
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-07-27 13:46:47 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 4.18 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,46 +1,52 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] mats, String[][] park) {
         int answer = 0;
 
         int row = park.length;
+
         int col = park[0].length;
 
-        int[][] dp = new int[row][col];
+        Arrays.sort(mats);
 
-        int maxSquare = 0;
+        for (int i = mats.length - 1; i >= 0; i--) {
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+            int size = mats[i];
 
-                if (!park[i][j].equals("-1")) {
-                    dp[i][j] = 0;
-                    continue;
+            for (int startRow = 0; startRow <= row - size; startRow++) {
+                for (int startCol = 0; startCol <= col - size; startCol++) {
+                    
+                    boolean possible = true;
+
+                    for (int j = startRow; j < startRow + size; j++) {
+                        
+                        if (!possible) {
+                            break;
+                        }
+
+                        for (int k = startCol; k < startCol + size; k++) {
+
+                            if (!park[j][k].equals("-1")) {
+                                possible = false;
+                                break;
+                            }
+
+                        }
+                        
+                    }
+
+                    if (possible) {
+                        return size;
+                    }
+                    
                 }
 
 
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                } else {
-
-                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
-
-                }
-
-                maxSquare = Math.max(dp[i][j], maxSquare);
-
-            }
-        }
-
-        answer = -1;
-
-        for (int mat : mats) {
-
-            if (mat <= maxSquare) {
-                answer = Math.max(answer, mat);
             }
 
         }
 
-        return answer;
+        return -1;
     }
 }
