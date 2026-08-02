c311906fd980d6c73aa435916203cb21e30291ea
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-02 23:13:24 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 10.15 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,80 +1,58 @@
 class Solution {
-    public static String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String answer = "";
 
         int videoSec = toSec(video_len);
 
-        int posSec = toSec(pos);
+        int currSec = toSec(pos);
 
         int opStartSec = toSec(op_start);
 
         int opEndSec = toSec(op_end);
 
+        if (currSec >= opStartSec && currSec <= opEndSec) {
+            currSec = opEndSec;
+        }
+
         for (String command : commands) {
-            
-            if (posSec >= opStartSec && posSec <= opEndSec) {
-                posSec = opEndSec;
-            }
 
             if (command.equals("next")) {
-                posSec += 10;
+                currSec = Math.min(currSec + 10, videoSec);
+            } else if (command.equals("prev")) {
 
-                if (posSec >= videoSec) {
-                    posSec = videoSec;
-                }
-            } else {
-                posSec -= 10;
-
-                if (posSec <= 0) {
-                    posSec = 0;
-                }
+                currSec = Math.max(0, currSec - 10);
             }
 
-            if (posSec >= opStartSec && posSec <= opEndSec) {
-                posSec = opEndSec;
+
+            if (currSec >= opStartSec && currSec <= opEndSec) {
+                currSec = opEndSec;
             }
 
         }
 
+        answer = toTime(currSec);
+
+        return answer;
+    }
+
+    private String toTime(int currSec) {
+
+        int m = currSec / 60;
+        int s = currSec % 60;
 
 
-        return toTime(posSec);
+        return String.format("%02d:%02d", m, s);
     }
 
 
-    public static int toSec(String time) {
+    public int toSec(String time) {
 
         String[] times = time.split(":");
 
+        int m = Integer.parseInt(times[0]);
+        int s = Integer.parseInt(times[1]);
 
-        return Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
+        return m * 60 + s;
 
     }
-
-    public static String toTime(int sec) {
-
-        if (sec == 0) {
-            return "00:00";
-        }
-
-        int m = sec / 60;
-        int s = sec % 60;
-
-        return String.format("%02d:%02d", m,s);
-    }
-
-
-    public static void main(String[] args) {
-        String video_len = "10:55";
-
-        String pos = "00:05";
-        String op_start = "00:15";
-        String op_end = "06:55";
-
-        String[] commands = {"prev", "next", "next"};
-
-        System.out.println(solution(video_len, pos, op_start, op_end,commands));
-    }
-
-
 }
