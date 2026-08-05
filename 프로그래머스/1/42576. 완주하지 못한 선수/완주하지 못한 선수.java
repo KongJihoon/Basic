0e62c0202c396b4941d54ad3c7bc5a531b86475d
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-05 17:03:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.78 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,24 +5,28 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
 
-        HashMap<String, Integer> map = new HashMap<>();
-
+        Map<String, Integer> map = new HashMap<>();
+        
         for (String key : participant) {
+            
             map.put(key, map.getOrDefault(key, 0) + 1);
+            
         }
+        
+        for (String item : completion) {
 
-
-        for (String key : completion) {
-
-            int n = map.get(key) - 1;
-
+            int n = map.get(item) - 1;
+            
             if (n == 0) {
-                map.remove(key);
+                map.remove(item);
             } else {
-                map.put(key, n);
+                map.put(item, n);
             }
+
         }
 
-        return map.keySet().iterator().next();
+        answer = map.keySet().iterator().next();
+
+        return answer;
     }
 }
