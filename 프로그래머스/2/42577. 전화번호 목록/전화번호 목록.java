9dfb49737ec91e7edc8e46fc1c14bc99515764fb
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-09 16:53:53 +0900

    [level 2] Title: 전화번호 목록, Time: 177.81 ms, Memory: 235 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,28 +1,25 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
     public boolean solution(String[] phone_book) {
 
-
-        Set<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
         
-        for (String phone : phone_book) {
-            set.add(phone);
-        }
         
         for (String phone : phone_book) {
 
             for (int i = 1; i < phone.length(); i++) {
                 
-                if (set.contains(phone.substring(0,i))) {
+                if (set.contains(phone.substring(0, i))) {
                     return false;
                 }
                 
             }
+            
         }
-
-
+        
         return true;
     }
 }
