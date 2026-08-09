cc1ff18785af69488247313740de15a64760ca4c
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-09 16:26:43 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 7.33 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -7,23 +7,24 @@ class Solution {
     public int solution(String message, int[][] spoiler_ranges) {
         int answer = 0;
 
-        int messageLen = message.length();
-
+        // 스포일러 구간 문자 담을 배열 생성
+        // 두 단어가 스포일러 방지 구간에 걸쳐있을 수 있다.
         List<List<String>> spoilerWords = new ArrayList<>();
 
         for (int i = 0; i < spoiler_ranges.length; i++) {
+
             spoilerWords.add(new ArrayList<>());
         }
 
-        int index = 0;
-
         Set<String> normalWords = new HashSet<>();
 
-        while (index < messageLen) {
+        int index = 0;
+
+        while (index < message.length()) {
 
             int startIndex = index;
 
-            while (index < messageLen && message.charAt(index) != ' ') {
+            while (index < message.length() && message.charAt(index) != ' ') {
                 index++;
             }
 
@@ -35,48 +36,51 @@ class Solution {
 
             for (int i = 0; i < spoiler_ranges.length; i++) {
 
-                int rangeStart = spoiler_ranges[i][0];
-                int rangeEnd = spoiler_ranges[i][1];
+                int spoilerStart = spoiler_ranges[i][0];
+                int spoilerEnd = spoiler_ranges[i][1];
 
-                if (isOverlap(startIndex, endIndex, rangeStart, rangeEnd)) {
+                if (isOverlap(startIndex, endIndex, spoilerStart, spoilerEnd)) {
+
                     isSpoiler = true;
-
                     spoilerWords.get(i).add(word);
                 }
+
             }
-            
+
             if (!isSpoiler) {
                 normalWords.add(word);
             }
-            
-            index++;
 
+            index++;
         }
-        
-        Set<String> confirmWords = new HashSet<>();
-        
+
+        // 중복 단어 검사
+        Set<String> confirmWord = new HashSet<>();
+
         for (List<String> words : spoilerWords) {
-            
+
             for (String word : words) {
-                
-                boolean isImportant = !normalWords.contains(word) && !confirmWords.contains(word);
-                
-                if (isImportant) {
+
+                boolean isImportantWord = !normalWords.contains(word) && !confirmWord.contains(word);
+
+                if (isImportantWord) {
                     answer++;
                 }
+
+                confirmWord.add(word);
                 
-                confirmWords.add(word);
-                
+
             }
-            
         }
+
 
 
         return answer;
     }
 
-    private boolean isOverlap(int wordStart, int wordEnd, int rangeStart, int rangeEnd) {
-
-        return Math.max(wordStart, rangeStart) <= Math.min(wordEnd, rangeEnd);
+    private boolean isOverlap(int startIndex, int endIndex, int spoilerStart, int spoilerEnd) {
+        return Math.max(startIndex, spoilerStart) <= Math.min(endIndex, spoilerEnd);
     }
+
+
 }
