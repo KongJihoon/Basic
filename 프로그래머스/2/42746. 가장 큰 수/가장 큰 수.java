9127698d9d1485677739dadfde3f0a4a8aabe9a9
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-16 16:52:55 +0900

    [level 2] Title: 가장 큰 수, Time: 127.27 ms, Memory: 137 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+
+        String[] answer = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            answer[i] = String.valueOf(numbers[i]);
+        }
+
+
+        Arrays.sort(answer, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (answer[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : answer) {
+            sb.append(s);
+        }
+
+
+        return sb.toString();
+
+    }
+}
