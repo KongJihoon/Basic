8890e51083b737451d7542a0b9883446019ba34e
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-18 17:34:54 +0900

    [level 2] Title: H-Index, Time: 5.44 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+        
+        int len = citations.length;
+
+        for (int i = 0; i < len; i++) {
+            
+            
+            if (citations[i] >= len - i) {
+                answer = len - i;
+                break;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
