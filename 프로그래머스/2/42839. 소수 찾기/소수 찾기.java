5749d29fa02054a141d6aef46040eb5572247611
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-20 14:02:59 +0900

    [level 2] Title: 소수 찾기, Time: 16.06 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,69 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    Set<Integer> set = new HashSet<>();
+
+    boolean[] visited;
+
+    public int solution(String numbers) {
+        int answer = 0;
+
+        visited = new boolean[numbers.length()];
+        
+        String current = "";
+        
+        dfs(current, numbers);
+        
+        for (int num : set) {
+            if (isPrime(num)) {
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+
+
+    public void dfs(String current, String numbers) {
+
+        for (int i = 0; i < numbers.length(); i++) {
+
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+
+            String next = current + numbers.charAt(i);
+
+            set.add(Integer.parseInt(next));
+
+            dfs(next, numbers);
+
+            visited[i] = false;
+
+        }
+
+    }
+
+    public boolean isPrime(int num) {
+
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+
+            if (num % i == 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+}
