a702e557f43e7f3540b043e62ad46489b3361b57
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-23 18:06:46 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,23 +2,23 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
 
+
         int area = brown + yellow;
 
-        for (int height = 3; height <= Math.sqrt(area) ; height++) {
-
+        for (int height = 3; height <= Math.sqrt(area); height++) {
+            
             if (area % height == 0) {
-
+                
                 int width = area / height;
-
+                
                 if ((width - 2) * (height - 2) == yellow) {
-                    answer[0] = Math.max(width, height);
-                    answer[1] = Math.min(width, height);
+                    answer[0] = width;
+                    answer[1] = height;
                     break;
                 }
-
+                
             }
-
-
+            
         }
 
 
