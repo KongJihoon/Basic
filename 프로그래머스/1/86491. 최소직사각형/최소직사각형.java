a47c3625c23358067fce1c7f971627b8e15e1e6a
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-08-23 17:54:19 +0900

    [level 1] Title: 최소직사각형, Time: 0.79 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,31 +1,22 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
         
+        int maxWidth = 0;
         
-        
-        // 최댓값 중 가장 큰 값과 최소값 중 가장 큰 값을 이용
-        // 위 두값을 이용하면 모든 명함을 넣을 수 있는 가장 작은 지갑
-        int len = sizes.length;
-        
-        int[] maxSize = new int[len];
-        int[] minSize = new int[len];
-        
+        int maxHeight = 0;
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < sizes.length; i++) {
             
-            maxSize[i] = Math.max(sizes[i][0], sizes[i][1]);
-            minSize[i] = Math.min(sizes[i][0], sizes[i][1]);
+            int maxValue = Math.max(sizes[i][0], sizes[i][1]);
+            int minValue = Math.min(sizes[i][0], sizes[i][1]);
+            
+            maxWidth = Math.max(maxValue, maxWidth);
+            maxHeight = Math.max(minValue, maxHeight);
             
         }
-
-        Arrays.sort(maxSize);
-        Arrays.sort(minSize);
         
-        // 오름차순 정렬 후 가장 큰 값의 곱을 리턴
-        answer = maxSize[len - 1] * minSize[len - 1];
+        answer = maxWidth * maxHeight;
         
         return answer;
     }
