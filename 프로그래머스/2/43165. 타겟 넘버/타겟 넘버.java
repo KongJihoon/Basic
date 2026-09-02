fdb46843bae0c781a96f41e0496a47eee6dbd8a3
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-09-02 16:13:41 +0900

    [level 2] Title: 타겟 넘버, Time: 3.46 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,35 +1,28 @@
 class Solution {
-
-    private static int count;
-
-    public static int solution(int[] numbers, int target) {
-
+    
+    int answer;
+    
+    public int solution(int[] numbers, int target) {
+        
         dfs(0, 0, numbers, target);
-
-        return count;
+        return answer;
     }
-
-
-    public static void dfs(int flag, int sum, int[] numbers, int target) {
-
+    
+    
+    public void dfs(int flag, int cur, int[] numbers, int target) {
+        
         if (flag == numbers.length) {
-            if (target == sum) {
-                count++;
+            
+            if (cur == target) {
+                answer++;
             }
+            
             return;
         }
-
-
-        dfs(flag + 1, sum + numbers[flag], numbers, target);
-        dfs(flag + 1, sum - numbers[flag], numbers, target);
-
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = {4, 1, 2, 1};
-
-        int target = 4;
-
-        System.out.println(solution(numbers, target));
+        
+        
+        dfs(flag + 1, cur + numbers[flag], numbers, target);
+        dfs(flag + 1, cur - numbers[flag], numbers, target);
+        
     }
 }
