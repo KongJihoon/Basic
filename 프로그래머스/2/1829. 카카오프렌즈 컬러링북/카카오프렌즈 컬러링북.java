4ed83d549803ee10cb09a1a02f7692e4a61b1f91
KongJiHoon <138794635+KongJihoon@users.noreply.github.com>
2026-09-13 23:04:36 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 10.54 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -3,22 +3,19 @@ import java.util.Queue;
 
 class Solution {
 
-
     boolean[][] visited;
 
     int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
+    int[] dy = {0, 0,-1, 1};
 
     public int[] solution(int m, int n, int[][] picture) {
         int numberOfArea = 0;
         int maxSizeOfOneArea = 0;
 
-
         visited = new boolean[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
 
                 if (picture[i][j] == 0) {
                     continue;
@@ -27,81 +24,69 @@ class Solution {
                 if (visited[i][j]) {
                     continue;
                 }
-
+                
                 numberOfArea++;
 
-                int ariaSize = bfs(i, j, picture,m,n);
+                int areaSize = bfs(m, n, i, j, picture);
 
-                maxSizeOfOneArea = Math.max(ariaSize, maxSizeOfOneArea);
-
+                maxSizeOfOneArea = Math.max(areaSize, maxSizeOfOneArea);
+                
             }
         }
-
 
         int[] answer = new int[2];
         answer[0] = numberOfArea;
         answer[1] = maxSizeOfOneArea;
-
-
-
         return answer;
     }
 
-    private int bfs(int startX, int startY, int[][] picture, int m, int n) {
-
-        int color = picture[startX][startY];
-
-        visited[startX][startY] = true;
+    private int bfs(int m, int n, int startX, int startY, int[][] picture) {
 
         Queue<int[]> queue = new ArrayDeque<>();
-
-        queue.offer(new int[]{startX, startY});
-
+        
+        int color = picture[startX][startY];
+        
         int count = 0;
-
-
+        
+        visited[startX][startY] = true;
+        
+        queue.offer(new int[]{startX, startY});
+        
         while (!queue.isEmpty()) {
-
             int[] cur = queue.poll();
-
+            
             int x = cur[0];
             int y = cur[1];
-
+            
             count++;
 
-
             for (int i = 0; i < 4; i++) {
-
+                
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
-                // 범위를 벗어 나는 경우
+                
                 if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
                     continue;
                 }
-
-                // 이미 방문한 경우
+                
+                if (picture[nx][ny] != color) {
+                    continue;
+                }
+                
                 if (visited[nx][ny]) {
                     continue;
                 }
-
-                // 같은 색깔이 아닌경우
-                if (color != picture[nx][ny]) {
-                    continue;
-                }
-
-
-
+                
                 visited[nx][ny] = true;
-
+                
                 queue.offer(new int[]{nx, ny});
-
-
-
+                
+                
             }
-
+            
         }
-
+        
+        
         return count;
     }
 }
